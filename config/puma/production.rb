803ae72c74c769cef9e0cdc93f1e5fa1be53ac77
Rabin Poudyal <rabinpoudyal1995@gmail.com>
2023-07-04 21:52:40 +1000

    fix sockets

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -3,11 +3,11 @@
 rails_env = "production"
 environment rails_env
 
-app_dir = "/home/deploy/deploy/current" # Update me with your root rails app path
+app_dir = "/home/deploy/deploy/shared" # Update me with your root rails app path
 
-bind  "unix://#{app_dir}/puma.sock"
-pidfile "#{app_dir}/puma.pid"
-state_path "#{app_dir}/puma.state"
+bind  "unix://#{app_dir}/tmp/sockets/puma.sock"
+pidfile "#{app_dir}/tmp/puma.pid"
+state_path "#{app_dir}/tmp/puma.state"
 directory "#{app_dir}/"
 
 stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
@@ -15,6 +15,6 @@ stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.lo
 workers 2
 threads 1,2
 
-activate_control_app "unix://#{app_dir}/pumactl.sock"
+activate_control_app "unix://#{app_dir}/tmp/shared/pumactl.sock"
 
 prune_bundler
