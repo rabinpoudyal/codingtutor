46f26d5fd688eb998f7dae5d384bcc2608dba39d
Rabin Poudyal <rabinpoudyal1995@gmail.com>
2023-07-05 22:16:57 +1000

    fix hosts

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Codingtutor
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.hosts << "codingtutor.online"
+    config.hosts << "170.64.165.188"
   end
 end
