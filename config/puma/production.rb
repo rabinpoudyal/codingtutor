2f66c5e3e23b09e59251cbb0c960150af0c258a5
Rabin Poudyal <rabinpoudyal1995@gmail.com>
2023-07-05 21:10:47 +1000

    fix puma

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -3,10 +3,10 @@ environment 'production'
 workers 2
 threads 1, 6
 
-app_dir = File.expand_path("../../../..", __FILE__)
+app_dir = File.expand_path("../../../../", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
-bind "unix:/#{shared_dir}/tmp/sockets/puma.sock"
+bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
 pidfile "#{shared_dir}/tmp/pids/puma.pid"
 state_path "#{shared_dir}/tmp/pids/puma.state"
 
