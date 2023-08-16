c0600330b097a466b90461f15b838e7535be8f16
Rabin Poudyal <rabinpoudyal1995@gmail.com>
2023-08-16 21:24:05 +1000

    fix contact form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,8 +12,21 @@ class HomeController < ApplicationController
   end
 
   def contacts
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to root_path, notice: "Your message has been sent."
+    else
+      redirect_to root_path, notice: "Your message has not been sent."
+    end
   end
 
   def abouat
   end
+
+  private 
+
+  def contact_params
+    params.permit(:name, :email, :message)
+  end
+
 end
